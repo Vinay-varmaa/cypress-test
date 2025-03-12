fa6964560a223f5577499daec8bc75540a37b535
Varma <vinayvini536@gmail.com>
2025-03-12 10:58:59 +0530

    Second commit

---
+++ cypress/support/command.ts
@@ -0,0 +1,9 @@
+Cypress.Commands.add('getByData', (selector: string) => {
+    return cy.get(`[data-test=${selector}]`);
+});
+
+/// <reference types="cypress" />
+
+Cypress.Commands.add('pathname', (expectedpathname: string) => {
+    return cy.location('pathname').should('eq', expectedpathname);
+});
